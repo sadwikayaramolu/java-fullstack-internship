9c9f1ce0bb9a91632b077fc35695a62475f5ceb4
sadwika <ssadwikayaramolu@gmail.com>
2025-06-09 16:02:16 +0530

    Add files via upload

---
+++ IncrementDecre.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class IncrementDecre
+{
+	public static void main(String args[])
+	 {
+    		int a = 5, b = 5;
+
+    		int x = ++a + ++a;  // Pre-increment
+    		int y = a++  + a++;  // Post-increment
+		System.out.println(x);
+		System.out.println(y);
+	
+	
+	}
+
+}
